ced9785d21c3f9e1d5816fc1eec905ded3ed3945
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-09 10:24:45 +0530

    Refactered: single Responsblity principal to build csv file

---
+++ CensusAnalyser/CSVBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    //build csv file
+    public class CSVBuilder
+    {
+        public static DataTable LoadCSVData(string CSVFilePath)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(CSVFilePath))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+                return csvData;
+            }
+            catch (FileNotFoundException)
+            {
+                throw new CensusAnalyzerException(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND, "");
+            }
+            catch (ArgumentNullException)
+            {
+                throw new CensusAnalyzerException(CensusAnalyzerException.ExceptionType.EMPTY_FILE, "");
+            }
+        }
+    }
+}
--- CensusAnalyser/CensusAnalyserManager.cs
+++ CensusAnalyser/CensusAnalyserManager.cs
@@ -10,48 +10,14 @@ namespace CensusAnalyser
         //loading India census data
         public static int LoadIndiaCensusData(string indianCensusCSVFilePath)
         {
-            DataTable csvDataTable = LoadCSVData( indianCensusCSVFilePath);
+            DataTable csvDataTable = CSVBuilder.LoadCSVData( indianCensusCSVFilePath);
             return csvDataTable.Rows.Count;
         }
         //load india state census data
         public static int LoadIndiaStateCode(string indianStateCensusCSVFilePath)
         {
-            DataTable csvDataTable=LoadCSVData(indianStateCensusCSVFilePath);
+            DataTable csvDataTable= CSVBuilder.LoadCSVData(indianStateCensusCSVFilePath);
             return csvDataTable.Rows.Count;
-        }
-        //load any csv data
-        public static DataTable LoadCSVData(string CSVFilePath)
-        {
-            DataTable csvData = new DataTable();
-            try
-            {
-                using (TextFieldParser csvReader = new TextFieldParser(CSVFilePath))
-                {
-                    csvReader.SetDelimiters(new string[] { "," });
-                    csvReader.HasFieldsEnclosedInQuotes = true;
-                    string[] colFields = csvReader.ReadFields();
-                    foreach (string column in colFields)
-                    {
-                        DataColumn datecolumn = new DataColumn(column);
-                        datecolumn.AllowDBNull = true;
-                        csvData.Columns.Add(datecolumn);
-                    }
-                    while (!csvReader.EndOfData)
-                    {
-                        string[] fieldData = csvReader.ReadFields();
-                        csvData.Rows.Add(fieldData);
-                    }
-                }
-                return csvData;
-            }
-            catch (FileNotFoundException)
-            {
-                throw new CensusAnalyzerException(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND, "");
-            }
-            catch (ArgumentNullException)
-            {
-                throw new CensusAnalyzerException(CensusAnalyzerException.ExceptionType.EMPTY_FILE, "");
-            }
         }
     }
 }
